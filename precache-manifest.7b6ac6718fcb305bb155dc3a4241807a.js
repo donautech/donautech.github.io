d6f2b78eba512043de2e7d22decb7559ec655842
dmi3coder <dmi3coder@gmail.com>
2019-07-27 11:19:22 +0300

    added missing js files

---
+++ precache-manifest.7b6ac6718fcb305bb155dc3a4241807a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c9cebe4d3615aff5dcf4ce4976ddd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd273fec9cb16ec4055",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bf1ad7ab12106d9824dd",
+    "url": "/static/js/2.46ce45ad.chunk.js"
+  },
+  {
+    "revision": "5cd273fec9cb16ec4055",
+    "url": "/static/js/main.078ed3c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
