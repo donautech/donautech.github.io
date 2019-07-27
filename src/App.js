143c798c4c759381c93993913f5681de8b9986da
dmi3coder <dmi3coder@gmail.com>
2019-07-27 11:07:58 +0300

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Navigation from "./components/Navigation";
 import MastHead from "./components/MastHead";
 import IconsGrid from "./components/IconsGrid";
@@ -7,15 +8,43 @@ import Showcase from "./components/ShowCase";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-      <div>
-        <Navigation/>
-        <MastHead/>
-        <IconsGrid/>
-        <Showcase/>
-        <Footer/>
-      </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Navigation/>
+                <Route exact path="/" component={MainScreen}/>
+                <Route path="/about" component={AboutScreen}/>
+                <Route path="/contacts" component={ContactsScreen}/>
+                <Footer/>
+            </div>
+        </Router>
+    );
+}
+
+function MainScreen() {
+    return (
+        <div>
+            <MastHead/>
+            <IconsGrid/>
+            <Showcase/>
+        </div>
+    )
+}
+
+function AboutScreen() {
+    return (
+        <div>
+        </div>
+    )
+
+}
+
+function ContactsScreen() {
+    return (
+        <div>
+        </div>
+    )
+
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Navigation extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light static-top">
                 <div className="container">
-                    <a className="navbar-brand" href="#">DonauTech</a>
-                    <div><a href="/about" style={{marginRight:10}}>About us</a><a className="btn btn-primary" href="mailto:info@donau.tech">Contact</a></div>
+                    <Link className="navbar-brand" to="/" >DonauTech</Link>
+                    <div>
+                        <Link to="/about" style={{marginRight:10}}>About us</Link>
+                        <Link to="/contacts" style={{marginRight:10}}>Contacts</Link>
+                        <a className="btn btn-primary"
+                           href="mailto:info@donau.tech?subject=I want free AI consultancy&body=Hello DonauTech!%0d%0dWe would like to receive free AI consultancy regarding my company!%0d%0d Please contact us by any of the contacts below:"
+                           style={{marginRight:10}}>Free AI consultancy</a>
+                    </div>
                 </div>
             </nav>
         );
