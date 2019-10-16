774d29c1984e35c1a28db5a87f3c4534a9ac77d9
dmi3coder <dmi3coder@gmail.com>
2019-10-16 11:26:23 +0200

    added missing files

---
+++ precache-manifest.4509e8c7269cf351be343e3bde66720e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f628d02e0404b6840067b7af34ca2970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3812c469931b5910ff7e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9cdfcc33606ec0413556",
+    "url": "/static/js/2.5e541c57.chunk.js"
+  },
+  {
+    "revision": "3812c469931b5910ff7e",
+    "url": "/static/js/main.0051b871.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
