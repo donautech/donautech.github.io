87d98b03c772daff0188cf9d464d2ee0e98be585
dmi3coder <dmi3coder@gmail.com>
2019-11-16 14:45:59 +0200

    Finished overview of projects, withoud description

--- src/v2/modules/views/ProductCategories.js
+++ src/v2/modules/views/ProductCategories.js
@@ -10,6 +10,8 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import {useTranslation} from "react-i18next";
+import Markdown from "../components/Markdown";
+import Button from "../components/Button";
 
 const styles = theme => ({
   root: {
@@ -117,124 +119,191 @@ function ProductCategories(props) {
   const images = [
     {
       url:
-          '/img/showcase-software-dev.jpg',
+        '/img/showcase-software-dev.jpg',
       title: 'Product/Software development',
       width: '40%',
-      description: ""
+      description: (<div>
+        <Typography variant="body1" gutterBottom>
+          We provide enterprise-grade software planning and development.
+        </Typography>
+        <Markdown>{
+          `* **Backend**: Spring, JavaEE, Quarkus, Netty, Jetty, Flask, Tensorflow, Neo4j, Laravel, NodeJS
+* **Mobile + PC**: Android SDK, JavaSE(6-10), Android Mobile, Wear, TV, JavaFX, IOS 
+* **DB**: SQL(SQLite,MySQL, PostgreSQL), Realm, ObjectBox, JavaFx, Elasticsearch,  MongoDB, Redis
+* **Blockchain**: Corda, Ethereum, Hyperledger
+* **Machine learning**: TensorFlow, PyTorch, Keras, Matplotlib, Seaborn, ggplot, Spacy, Scrapy, Selenium, Beautiful Soup 4, requests, Scikit-learn, Scipy, Numpy, Pandas, Cvxpy`.toString()
+        }
+        </Markdown>
+      </div>)
     },
     {
       url:
-          '',
-      title: 'TODO',
+        '/img/robot.jpg',
+      title: 'Machine Learning',
       width: '20%',
     },
     {
       url:
-          '/img/showcase-3.png',
+        '/img/showcase-3.png',
       title: 'Upcoming product ;)',
       width: '40%',
     },
     {
       url:
-          '/img/medium.png',
+        '/img/medium.png',
       title: 'Medium articles',
       width: '38%',
+      description: (<div>
+        <Typography variant="body1" gutterBottom>
+          We write articles at medium ranging from mobile development lifehack to Kubernetes and Machine Learning,
+          you should definitely check them out
+        </Typography>
+        <Button
+          color="secondary"
+          size="small"
+          variant="contained"
+          className={classes.button}
+          component="a"
+          href="https://medium.com/donautech"
+        >
+          Open articles
+        </Button>
+      </div>)
     },
     {
       url:
-          '/img/behaiv_usage.png',
+        '/img/behaiv_usage.png',
       title: 'Behaiv',
       width: '38%',
+      description: (<div>
+        <Typography variant="body1" gutterBottom>
+          Behaiv is a high-level AI tool that helps users to navigate faster through the app,
+          predict what users want to open and take appropriate actions.
+          It was developed with a focus on simplicity of usage.
+          Instead of overwhelming developers with burden of implementation,
+          they only need to write few lines of code.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Our company covers Java, Swift, Python and Javascript versions of this library
+        </Typography>
+        <Button
+          color="primary"
+          size="small"
+          variant="contained"
+          className={classes.button}
+          component="a"
+          href="https://github.com/dmi3coder/behaiv-java"
+        >
+          Look through our library
+        </Button>
+      </div>)
     },
     {
       url:
-          '',
-      title: 'TODO',
+        '/img/youtube.png',
+      title: 'YouTube Lessons',
       width: '24%',
+      description: (
+        <div>
+          <Typography variant="body1" gutterBottom>
+            We have small youtube channel where we put some lessons and discussions about different AI, ML and Software Engineering topics
+          </Typography>
+          <Button
+            color="primary"
+            size="small"
+            variant="contained"
+            className={classes.button}
+            component="a"
+            href="https://github.com/dmi3coder/behaiv-java"
+          >
+            Visit YouTube channel
+          </Button>
+        </div>
+      )
     },
     {
       url:
-          '/img/showcase-lovepons.png',
+        '/img/showcase-lovepons.png',
       title: 'Lovepons',
       width: '40%',
     },
     {
       url:
-          '',
-      title: 'TODO',
+        '/img/next_project.jpg',
+      title: 'Your next project?',
       width: '20%',
     },
     {
       url:
-          '/img/bg-showcase-1.jpg',
+        '/img/bg-showcase-1.jpg',
       title: 'IOT/Scooter Business services',
       width: '40%',
     },
   ];
 
   return (
-      <Container className={classes.root} component="section">
-        <Typography variant="h4" marked="center" align="center" component="h2">
-          {t('Our products and services')}
-        </Typography>
-        <div className={classes.images}>
-          {images.map(image => (
-              <ButtonBase
-                  key={image.title}
-                  className={classes.imageWrapper}
-                  onClick={() => handleClickOpen(image)}
-                  style={{
-                    width: image.width,
-                  }}
-              >
-                <div
-                    className={classes.imageSrc}
-                    style={{
-                      backgroundImage: `url(${image.url})`,
-                    }}
-                />
-                <div className={classes.imageBackdrop}/>
-                <div className={classes.imageButton}>
-                  <Typography
-                      component="h3"
-                      variant="h6"
-                      color="inherit"
-                      className={classes.imageTitle}
-                  >
-                    {t(image.title)}
-                    <div className={classes.imageMarked}/>
-                  </Typography>
-                </div>
-              </ButtonBase>
-          ))}
-        </div>
-        <Dialog
-            open={detailsDialogOpen}
-            maxWidth='md'
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={handleClickClose}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
-        >
-          <CardActionArea>
-            <CardMedia
-                className={classes.media}
-                image={selectedDialogImage.url}
-                title="Contemplative Reptile"
+    <Container className={classes.root} component="section">
+      <Typography variant="h4" marked="center" align="center" component="h2">
+        {t('Our products and services')}
+      </Typography>
+      <div className={classes.images}>
+        {images.map(image => (
+          <ButtonBase
+            key={image.title}
+            className={classes.imageWrapper}
+            onClick={() => handleClickOpen(image)}
+            style={{
+              width: image.width,
+            }}
+          >
+            <div
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${image.url})`,
+              }}
             />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {selectedDialogImage.title}
+            <div className={classes.imageBackdrop}/>
+            <div className={classes.imageButton}>
+              <Typography
+                component="h3"
+                variant="h6"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                {t(image.title)}
+                <div className={classes.imageMarked}/>
               </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Dialog>
-      </Container>
+            </div>
+          </ButtonBase>
+        ))}
+      </div>
+      <Dialog
+        open={detailsDialogOpen}
+        maxWidth='md'
+        fullWidth={true}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClickClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={selectedDialogImage.url}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {selectedDialogImage.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {selectedDialogImage.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Dialog>
+    </Container>
   );
 }
 
